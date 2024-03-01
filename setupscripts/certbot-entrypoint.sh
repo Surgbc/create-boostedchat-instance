c933940b5c42043c4e7cb426e38d5267261990f3
Brian Onang'o <surgbc@gmail.com>
2024-03-01 15:11:06 +0300

    Fix bugs

--- setupscripts/certbot-entrypoint.sh
+++ setupscripts/certbot-entrypoint.sh
@@ -7,3 +7,4 @@
 [ -f /etc/letsencrypt/live/scrapper.jamel.boostedchat.com/privkey.pem ] || certbot certonly --webroot --webroot-path=/usr/share/nginx/html --email tomek@boostedchat.com --agree-tos --no-eff-email --force-renewal -d scrapper.jamel.boostedchat.com
 [ -f /etc/letsencrypt/live/promptemplate.jamel.boostedchat.com/privkey.pem ] || certbot certonly --webroot --webroot-path=/usr/share/nginx/html --email tomek@boostedchat.com --agree-tos --no-eff-email --force-renewal -d promptemplate.jamel.boostedchat.com
 
+## comments

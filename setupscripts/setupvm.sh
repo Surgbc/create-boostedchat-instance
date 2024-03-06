c4016fd24ecc23ff8bdfe83f1c60580c7c927c9f
Brian Onang'o <surgbc@gmail.com>
2024-03-06 11:41:58 +0300

    Add nodes

--- setupscripts/setupvm.sh
+++ setupscripts/setupvm.sh
@@ -238,7 +238,7 @@ initialSetup() {
         sed -i "s/__GENERIC_STR3__/$random_string/g" .env
     done
 
-    source <(sed 's/^/export /' .env )  # is this really necessary, or does docker export the variables in .env by itself?
+    # source <(sed 's/^/export /' .env )  # is this really necessary, or does docker export the variables in .env by itself?
 
     # if [ "$BRANCH" == "dev" ]; then
     #     save_docker_yaml

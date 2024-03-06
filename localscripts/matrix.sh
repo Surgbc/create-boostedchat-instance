7e8cba4dbc3a2f53b9d16bb05a733af96661d2d3
Brian Onang'o <surgbc@gmail.com>
2024-03-06 17:42:08 +0300

    Fix scripts

--- localscripts/matrix.sh
+++ localscripts/matrix.sh
@@ -40,11 +40,21 @@ for section in "${sections[@]}"; do
         useService="$(echo $section | grep -o "^airflow\-" )$service"
 
         repoFull="fullRepo"
-        curl -s -H "Authorization: Bearer $PAT"  -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/$repoFull/branches/devs$useService-$innerBranch
+        # curl -s -H "Authorization: Bearer $PAT"  -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/$repoFull/branches/devs$useService-$innerBranch
         
+        response=$(curl -s -o /dev/null -w "%{http_code}" -H "Authorization: Bearer $PAT" -H "Accept: application/vnd.github.v3+json" "https://api.github.com/repos/$repoFull/branches/devs$useService-$innerBranch")
+        if [ $response -eq 200 ]; then
+            # Branch exists, proceed with adding data to config.json
+            echo "{\"service\":\"$useService\", \"image\":\"$image_name\", \"repo\":\"$repository\", \"branch\":\"$innerBranch\"  }" >> config.json
+        else
+            # Branch does not exist, skipping
+            echo "Branch devs$useService-$innerBranch does not exist. Skipping..."
+        fi
+
         echo "{\"service\":\"$useService\", \"image\":\"$image_name\", \"repo\":\"$repository\", \"branch\":\"$innerBranch\"  }">> config.json
         echo "," >> config.json
     done
 done
 
-awk 'NR>1 {print prev} {prev=$0} END {print "]}" }' config.json > temp && mv temp config.json # replace last ,
+# awk 'NR>1 {print prev} {prev=$0} END {print "]}" }' config.json > temp && mv temp config.json # replace last ,
+awk 'NR>1 {print prev} {prev=$0} END {sub(/,$/, "", prev); print prev "]}" }' config.json > temp && mv temp config.json  # replace last ,
